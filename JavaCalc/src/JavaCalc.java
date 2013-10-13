1fd83b2ed29ddb07a775f50d6ce7fc795873fe58
RednilLabs <rednillabs@gmail.com>
2013-10-13 12:56:43 -0700

    Update JavaCalc.java

--- JavaCalc/src/JavaCalc.java
+++ JavaCalc/src/JavaCalc.java
@@ -1,4 +1,3 @@
-
 public class JavaCalc {
 
 	/**
@@ -6,7 +5,7 @@ public class JavaCalc {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-// change 3
+// change 4 from GH.
 	}
 
 }
